cf49b76689eb35042653a1c86a292c176696a811
mubeen <mubeenayub123@gmail.com>
2022-10-30 03:57:45 +0500

    add emailing options 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,9 +14,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'mighty-dawn-74746.herokuapp.com',
+    domain:               'gmail.com',
     user_name:            'fyp34b@gmail.com',
-    password:             '2113e249',
+    password:             'pjsbtbiwczkxotzo',
     authentication:       'plain'
   }
 

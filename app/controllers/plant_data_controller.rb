bc1863adce392cfe981cf2c578f7f208605677ed
mubeen <mubeenayub123@gmail.com>
2022-10-30 21:50:57 +0500

    allow uuid

--- app/controllers/plant_data_controller.rb
+++ app/controllers/plant_data_controller.rb
@@ -7,12 +7,18 @@ class PlantDataController < ApplicationController
   end
 
   def create
-    plant_data = PlantDatum.create(plant_data_params)
-    irrigation_system = IrrigationSystem.find_by(uuid: params[:plant_data][:irrigation_system_uuid])
+    plant_data = PlantDatum.create(
+      soil_moisture: plant_data_params[:soil_moisture],
+      humidity: plant_data_params[:humidity],
+      temprature: plant_data_params[:temprature],
+      light_intensity: plant_data_params[:light_intensity],
+      solenoid_walve: plant_data_params[:solenoid_walve],
+    )
+    irrigation_system = IrrigationSystem.find_by(uuid: plant_data_params[:irrigation_system_uuid])
     plant_data.update irrigation_system: irrigation_system
   end
 
   def plant_data_params
-    params.require(:plant_data).permit(:soil_moisture, :humidity, :temprature, :solenoid_walve, :light_intensity)
+    params.require(:plant_data).permit(:soil_moisture, :humidity, :temprature, :solenoid_walve, :light_intensity, :irrigation_system_uuid)
   end
 end

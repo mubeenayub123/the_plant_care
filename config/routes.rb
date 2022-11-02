1190f23cfb294a9e628c4da61fcc55f0ddf9199e
mubeen <mubeenayub123@gmail.com>
2022-11-02 15:07:44 +0500

    add export route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :plant_data
+  get 'export', to: 'plant_data#export', as: :export_plant_data
   resources :irrigation_systems
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

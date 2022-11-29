356be2929f483c73509e31d566ecfe8f6d340737
mubeen <mubeenayub123@gmail.com>
2022-11-29 22:32:31 +0500

    rollback routes debug

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,7 @@ Rails.application.routes.draw do
   resources :irrigation_systems
   resources :plants
   resources :dashboard
-  devise_for :users do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end 
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

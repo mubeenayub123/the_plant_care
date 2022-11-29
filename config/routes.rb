ce5488861ef383aa83f18953a0bad6f7f672e420
mubeen <mubeenayub123@gmail.com>
2022-11-29 22:26:14 +0500

    debug routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   resources :irrigation_systems
   resources :plants
   resources :dashboard
-  devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

8d9c81aaf74166dc204eb8bea46d7700b20037d0
mubeen <mubeenayub123@gmail.com>
2023-01-30 13:21:16 +0500

    use mini magick for variant processor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.active_storage.variant_processor = :mini_magick
+  #hello
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.perform_deliveries = true
